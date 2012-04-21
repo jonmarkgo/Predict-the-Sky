88740000894fd3dbb7fcc33e9f23489a12a729a1
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-04-21 10:51:05 -0400

    relationship between events and subscribers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   attr_accessible :name, :type
+  has_and_belongs_to_many :subscribers
 end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,4 @@
 class Subscriber < ActiveRecord::Base
   attr_accessible :latitude, :longitude, :phonenumber
+  has_and_belongs_to_many :events
 end
