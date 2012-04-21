c81ef1199fe30598658832fe736e05f1e736e6fc
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-04-21 10:30:30 -0400

    Initialized new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PredictTheSky::Application.config.secret_token = '690051780a510611cdd01fe8f61ac63aa99dd5ff9e38b40accf6c82b324590693ea81fc6c4ff5331a0cd09069df1642e992e2b2125c7754c1c6c708a3e52a450'
