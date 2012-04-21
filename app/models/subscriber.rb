d42d62af3659e4157360e335e952ab3af41338b5
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-04-21 10:52:46 -0400

    Geocode this mofo

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,6 @@
 class Subscriber < ActiveRecord::Base
   attr_accessible :latitude, :longitude, :phonenumber
   has_and_belongs_to_many :events
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode  # auto-fetch address
 end
