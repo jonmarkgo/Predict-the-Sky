1e5b870a1ba1d5dcf099cee04d2ef98e1074d286
Jeff Saracco <jeffsaracco@gmail.com>
2012-04-21 22:00:32 -0400

    give correct URL for ISS passes

--- app/models/iss_event.rb
+++ app/models/iss_event.rb
@@ -14,7 +14,7 @@ class IssEvent < Event
     if subscribers.count > 0
       @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
       subscribers.each do |subscriber|
-        doc = Hpricot(open("http://www.heavens-above.com/iridium.asp?Dur=1&lat="+subscriber.latitude.to_s+"&lng="+subscriber.longitude.to_s+"&tz="+subscriber.zone))
+        doc = Hpricot(open("http://www.heavens-above.com/PassSummary.aspx?satid=25544&Dur=1&lat="+subscriber.latitude.to_s+"&lng="+subscriber.longitude.to_s+"&tz="+subscriber.zone))
 
         count = 1
         start_time = alt = az = date = ''
@@ -38,7 +38,7 @@ class IssEvent < Event
 
         if (time_diff[:year] == 0 and time_diff[:month] == 0 and time_diff[:week] == 0 and time_diff[:day] == 0 and time_diff[:hour] == 0 and time_diff[:minute] < 20)
           puts 'Sending SMS to ' + subscriber.phonenumber + ' about the ISS'
-      
+
           @client.account.sms.messages.create(
             :from => ENV['TWILIO_FROM_NUMBER'],
             :to => subscriber.phonenumber,
